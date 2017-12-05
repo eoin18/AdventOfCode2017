f46f5cd2d2bd3a483754ee7336177462696d7d0d
eoin.mccarthy <eoin.mccarthy@bloombergpolarlake.com>
2017-12-05 13:21:13 +0000

    Day 5 Part 1 + 2

---
+++ day5/part1/offsets.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def main():
+    steps = 0
+    position = 0
+    instructions = []
+    with open('data.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            instructions.append(int(line.strip('\n')))
+    while position >= 0 and position < len(instructions):
+        temp = position
+        position += instructions[position]
+        instructions[temp] += 1
+        steps+= 1
+    print steps
+
+if __name__ == "__main__":
+    main()
